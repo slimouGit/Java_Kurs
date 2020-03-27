d5dc862ad3ed63e7f7ba2849d2c9d2600025c17d
Salim Oussayfi <box@slimou.net>
2020-03-27 17:35:43 +0100

    predicate examples

--- src/Predicates/PredicateExamples.java
+++ src/Predicates/PredicateExamples.java
@@ -1,8 +1,24 @@
 package Predicates;
 
-/**
- * User: salim
- * Date: 27.03.2020 17:32
- */
+import java.util.function.Predicate;
+
 public class PredicateExamples {
+    public static void main(String[] args) {
+        //Simple predicate example
+        Predicate<Integer> lesserthan = i -> (i < 20);
+        System.out.println(lesserthan.test(10));
+        System.out.println(lesserthan.test(100));
+
+        //two conditions => range
+        Predicate<Integer> greaterThanTen = (i) -> i > 10;
+        // Creating predicates
+        Predicate<Integer> lowerThanTwenty = (i) -> i < 20;
+
+        boolean result = greaterThanTen.and(lowerThanTwenty).test(15); //true
+        System.out.println(result);
+
+        boolean result2 = greaterThanTen.and(lowerThanTwenty).negate().test(15); //false => -15
+        System.out.println(result2);
+
+    }
 }
