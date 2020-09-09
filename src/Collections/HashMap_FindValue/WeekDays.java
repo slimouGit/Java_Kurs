b493ae9c10ced91e00432587c7ac1d025b5c3054
Salim Oussayfi <box@slimou.net>
2020-09-09 18:25:52 +0200

    LinkedHashMap counter

---
+++ src/Collections/HashMap_FindValue/WeekDays.java
@@ -0,0 +1,47 @@
+package Collections.HashMap_FindValue;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class WeekDays {
+
+    public enum DaysOfWeek {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRYDAY, SATURDAY, SUNDAY;
+    }
+
+
+    public static void main(String[] args) {
+        Map<String, DaysOfWeek> weekdays = new LinkedHashMap<>();
+        weekdays.put("Monday", DaysOfWeek.MONDAY);
+        weekdays.put("Tuesday", DaysOfWeek.TUESDAY);
+        weekdays.put("Wednesday", DaysOfWeek.WEDNESDAY);
+        weekdays.put("Thursday", DaysOfWeek.THURSDAY);
+        weekdays.put("Friday", DaysOfWeek.FRYDAY);
+        weekdays.put("Saturday", DaysOfWeek.SATURDAY);
+        weekdays.put("Sunday", DaysOfWeek.SUNDAY);
+
+        findEnumDay(weekdays, 5);
+    }
+
+    private static void findEnumDay(Map<String, DaysOfWeek> weekdays, int i) {
+        weekdays.entrySet().forEach(e -> {
+            System.out.println(e.hashCode() + "Key: " + e.getKey() + " Value: " + e.getValue());
+        });
+
+        weekdays.forEach((k, v) -> System.out.println("Item : " + k + " Count : " + v));
+
+
+        StringBuilder message = new StringBuilder("I like ");
+        int counter = 0;
+        for (Map.Entry<String, DaysOfWeek> item : weekdays.entrySet()) {
+            if (counter == i) {
+                message.append(item.getValue().toString());
+            }
+            counter++;
+        }
+        System.out.println(message);
+
+    }
+
+
+}
---
+++ src/Collections/HashMap_FindValue/WeekDaysTest.java
@@ -0,0 +1,16 @@
+package Collections.HashMap_FindValue;
+
+import static org.junit.Assert.*;
+
+/**
+ * User: salim
+ * Date: 09.09.2020 17:49
+ */
+public class WeekDaysTest {
+
+    public void testDays(){
+        WeekDays weekDays = new WeekDays();
+
+    }
+
+}
