1066ce6809bb0548f2d7ecddf42ab5b0b6669846
oussa <box@slimou.net>
2022-07-23 18:42:32 +0200

    print something before we transform the articles into article titles

--- src/Mapping/ArticleMapper.java
+++ src/Mapping/ArticleMapper.java
@@ -13,17 +13,17 @@ public class ArticleMapper {
                         "For Loop Java Tutorial",
                         "Dimitris Tasios"),
                 new Article(
-                        1,
-                        "For Loop Java Tutorial",
-                        "Dimitris Tasios"),
-                new Article(
                         2,
                         "Java Interface Tutorial",
                         "Georgios - Nikolaos Palaiologopoulos"),
                 new Article(
                         3,
                         "Java Generics Tutorial",
-                        "Ignis Dei")
+                        "Ignis Dei"),
+                new Article(
+                        4,
+                        "Math.random Java Tutorial",
+                        "Georgios - Nikolaos Palaiologopoulos")
         );
 
         printMapedTitle(mapToTitle(articles));
@@ -31,7 +31,10 @@ public class ArticleMapper {
 
     private static List<String> mapToTitle(List<Article> articles) {
         return articles.stream()
-                .map(Article::getTitle)
+                .map(article -> {
+                    System.out.println("This article \"" + article.getTitle() + "\" is written by: " +article.getAuthorName());
+                    return article.getTitle();
+                })
                 .collect(Collectors.toList());
     }
     private static void printMapedTitle(List<String> mapedToTitleList) {
