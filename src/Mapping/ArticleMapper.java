6fba440e1baed7ac76d340ed266cfa0da4373803
oussa <box@slimou.net>
2022-07-23 18:39:03 +0200

    shorten code in mapping example

--- src/Mapping/ArticleMapper.java
+++ src/Mapping/ArticleMapper.java
@@ -1,52 +1,41 @@
 package Mapping;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ArticleMapper {
 
-
     public static void main(String[] args) {
-        List<Article> articles = new ArrayList<>();
-        Article forLoop = new Article(
-                1,
-                "For Loop Java Tutorial",
-                "Dimitris Tasios"
+        List<Article> articles = Arrays.asList(
+                new Article(
+                        1,
+                        "For Loop Java Tutorial",
+                        "Dimitris Tasios"),
+                new Article(
+                        1,
+                        "For Loop Java Tutorial",
+                        "Dimitris Tasios"),
+                new Article(
+                        2,
+                        "Java Interface Tutorial",
+                        "Georgios - Nikolaos Palaiologopoulos"),
+                new Article(
+                        3,
+                        "Java Generics Tutorial",
+                        "Ignis Dei")
         );
-        Article interfaceTutorial = new Article(
-                2,
-                "Java Interface Tutorial",
-                "Georgios - Nikolaos Palaiologopoulos"
-        );
-        Article generics = new Article(
-                3,
-                "Java Generics Tutorial",
-                "Ignis Dei"
-        );
-        Article mathRandom = new Article(
-                4,
-                "Math.random Java Tutorial",
-                "Georgios - Nikolaos Palaiologopoulos"
-        );
-        articles.add(forLoop);
-        articles.add(interfaceTutorial);
-        articles.add(generics);
-        articles.add(mathRandom);
-        List<String> mapedToTitleList = mapToTitle(articles);
 
-        printMapedTitle(mapedToTitleList);
+        printMapedTitle(mapToTitle(articles));
     }
+
     private static List<String> mapToTitle(List<Article> articles) {
         return articles.stream()
                 .map(Article::getTitle)
                 .collect(Collectors.toList());
-//        return articles.stream()
-//                .map(article -> article.getTitle())
-//                .collect(Collectors.toList());
     }
     private static void printMapedTitle(List<String> mapedToTitleList) {
-        mapedToTitleList.forEach(title->System.out.println(title));
+        mapedToTitleList.forEach(title -> System.out.println(title));
     }
 
 }
